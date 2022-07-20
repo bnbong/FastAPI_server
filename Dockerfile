a58910145bfb055e411bd0cf377e871a3ce6cebb
bnbong <bbbong9@gmail.com>
2022-07-20 18:43:40 +0900

    modify settings

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM python:3.8
 
 WORKDIR /app
 
-COPY ./requirements.txt /code/requirements.txt
+COPY ./app/requirements.txt /app/requirements.txt
 
-RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 
 COPY ./app /app
 
--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 from db.database import SessionLocal, engine
-from app.accounts import models, crud, schemas
+from accounts import models, crud, schemas
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -24,10 +25,14 @@ def get_db():
 def read_root():
     return {"Hello": "World"}
 
-# @app.get("/users/{user_id}", response_model=schemas.User)
-# def read_user(user_id: int, db: Session = Depends(get_db)):
-#     db_user = crud.get_user(db, user_id=user_id)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="User not found.")
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found.")
 
-#     return db_user
+    return db_user
+
+
+# if __name__ == '__main__':
+#     uvicorn.run("main:app", "--proxy-headers", host="serverside-testing.bnbongcodeserver.tk", port=8000)
