9f5e6662796f98f4ae4c6589baf192d4947608c6
bnbong <bbbong9@gmail.com>
2022-07-25 19:53:58 +0900

    update server

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import uvicorn
+
+from typing import List
 from fastapi import FastAPI
 
 from fastapi import Depends, FastAPI, HTTPException
@@ -24,6 +26,20 @@ def get_db():
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+
+    return users
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    return crud.create_user(db=db, user=user)
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
--- app/test.py
+++ app/test.py
@@ -11,6 +11,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
 import os
+import hashlib
 
 
 load_dotenv()
@@ -18,6 +19,7 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URL = 'postgresql://' + \
     os.getenv('PG_USER') + ':' + os.getenv('PG_PASSWORD') +\
         '@' + os.getenv('PG_HOSTNAME') + ':' + os.getenv('PG_PORT') + '/' + os.getenv('PG_DBNAME')
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db" 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -35,6 +37,15 @@ app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
+def hashing_text():
+    test_password = "thepasswordtext"
+    test_text = test_password + "thiswillbehashed"
+    test_text = test_text.encode()
+    h = hashlib.sha256()
+    h.update(test_text)
+    hashed_text = h.hexdigest()
+
+    return hashed_text
 
 def test_is_dotenv_work():
     from dotenv import load_dotenv
@@ -46,6 +57,12 @@ def test_is_dotenv_work():
 
     assert 'fastapiserverpostgresqldb' == pgadmin_dbname
 
+def test_is_hashing_function_work():
+    test_text = "thepasswordtext" + "thiswillbehashed"
+    hashed_text = hashing_text()
+    
+    assert not hashed_text == test_text
+
 def test_read_root():
     response = client.get('/')
     
@@ -56,4 +73,4 @@ def test_db():
     response = client.get('/users/1')
 
     assert 200 == response.status_code
-    assert {"email":"testemail@test.com","id":1,"is_active":True} == response.json()
+    assert {"email":"test@testmail.com","id":1,"is_active":True} == response.json()
