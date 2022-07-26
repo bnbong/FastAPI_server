050199d6f38a55cfb26b1e0b7b83eb165e3da5a3
bnbong <bbbong9@gmail.com>
2022-07-26 13:14:49 +0900

    making accounts app put method

--- app/main.py
+++ app/main.py
@@ -48,3 +48,11 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found.")
 
     return db_user
+
+@app.put("/users/{user_id}", response_model=schemas.User)
+def update_user(user_id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found.")
+
+    return crud.update_user(db=db, user=user)
